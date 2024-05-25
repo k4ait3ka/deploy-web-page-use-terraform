cccd9247f4c98f9e88388d0c5b98312bd2cbe9ae
khaitk <khaitkdev@gmail.com>
2024-05-25 20:12:23 +0700

    Deploy web page using terraform

---
+++ terraform.tfvars
@@ -0,0 +1,3 @@
+access_key = ""         
+secret_key = ""
+bucket_name = "app25052024"		
---
+++ variables.tf
@@ -0,0 +1,13 @@
+# required for AWS
+variable "access_key" {
+    description = ""
+}
+variable "secret_key" {
+    description = ""
+}
+variable "region" {
+    default = "us-east-1"
+}
+variable "bucket_name" {
+    default = "app25052024"
+}				
